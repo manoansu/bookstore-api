7c451d9f975d21cbb7b920b1d877431aba72ed15
Ansumane Mané <manoansu@gmail.com>
2021-05-26 13:15:16 +0100

    Patch para o livro

--- src/main/java/pt/amane/bookstore/resources/LivroResource.java
+++ src/main/java/pt/amane/bookstore/resources/LivroResource.java
@@ -1,5 +1,6 @@
 package pt.amane.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,11 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import pt.amane.bookstore.domain.Livro;
 import pt.amane.bookstore.dtos.LivroDTO;
@@ -32,44 +35,61 @@ public class LivroResource {
 		return ResponseEntity.ok().body(obj); // apenas retorna o objecto livro que vem con o tratamento de excessão
 												// null ou retorna o id
 	}
-	
-	//http://localhgost:8080/livros?categoria=1 ==> esse é url para listar determinada categoria de um determinado livros
-	@GetMapping 
-	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue="0") Integer id_cat){
-		
-		List<Livro> list = ls.findAll(id_cat);// retorana lista de livro de uma determinada categoria vindo da interface livroRepository..
-		
-		//Converte a lista de livro  para lista de livros..
-		List<LivroDTO> listDto = list.stream().map(obj ->new LivroDTO(obj)).collect(Collectors.toList());
+
+	// http://localhgost:8080/livros?categoria=1 ==> esse é url para listar
+	// determinada categoria de um determinado livros
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll(
+			@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat) {
+
+		List<Livro> list = ls.findAll(id_cat);// retorana lista de livro de uma determinada categoria vindo da interface
+												// livroRepository..
+
+		// Converte a lista de livro para lista de livros..
+		List<LivroDTO> listDto = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
-		
+
 	}
-	
+
 	/**
-	 * para atualizar preciso receber o id para fazer a busca na BD e verificar se object existe ou não.
-	 * E tambem tenho que ter informação atualizada que vai vir no corpo de requisição que 
-	 * chamamos de requestBody sendo um objecto json do tipo livro nesse caso.. 
+	 * para atualizar preciso receber o id para fazer a busca na BD e verificar se
+	 * object existe ou não. E tambem tenho que ter informação atualizada que vai
+	 * vir no corpo de requisição que chamamos de requestBody sendo um objecto json
+	 * do tipo livro nesse caso..
+	 * 
 	 * @param id
 	 * @param obj
 	 * @return
 	 */
-	@PutMapping(value = "/{id}")  // endpoint id
-	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj){
-		
-		Livro newObj = ls.update(id,obj); // o objecto newobj recebe o livro atualizado na classe LivroSewrvice..
-		
-		// uma vez que newObj veio atualizado de classe LivroService, é so retornar o newObj.
+	@PutMapping(value = "/{id}") // endpoint id
+	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj) {
+
+		Livro newObj = ls.update(id, obj); // o objecto newobj recebe o livro atualizado na classe LivroSewrvice..
+
+		// uma vez que newObj veio atualizado de classe LivroService, é so retornar o
+		// newObj.
 		return ResponseEntity.ok().body(newObj);
-		
+
 	}
-	
-	@PatchMapping(value = "/{id}")  // endpoint id
-	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj){
-		
-		Livro newObj = ls.update(id,obj); // o objecto newobj recebe o livro atualizado na classe LivroSewrvice..
-		
-		// uma vez que newObj veio atualizado de classe LivroService, é so retornar o newObj.
+
+	@PatchMapping(value = "/{id}") // endpoint id
+	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj) {
+
+		Livro newObj = ls.update(id, obj); // o objecto newobj recebe o livro atualizado na classe LivroSewrvice..
+
+		// uma vez que newObj veio atualizado de classe LivroService, é so retornar o
+		// newObj.
 		return ResponseEntity.ok().body(newObj);
-		
+	}
+
+	@PostMapping
+	public ResponseEntity<Livro> create(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat,
+			@RequestBody Livro obj) {
+
+		Livro newobj = ls.create(id_cat, obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}")
+				.buildAndExpand(newobj.getId()).toUri();// retorna o URI para user.. 
+		return ResponseEntity.created(uri).build();
+
 	}
 }
--- src/main/java/pt/amane/bookstore/service/LivroService.java
+++ src/main/java/pt/amane/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import pt.amane.bookstore.domain.Categoria;
 import pt.amane.bookstore.domain.Livro;
 import pt.amane.bookstore.repositories.LivroRepository;
 import pt.amane.bookstore.service.exceptions.ObjectNotFoundException;
@@ -17,7 +18,7 @@ public class LivroService {
 	private LivroRepository lr;
 
 	@Autowired
-	private CategoriaService cr;
+	private CategoriaService cs;
 
 	public Livro findById(Integer id) {
 
@@ -27,7 +28,7 @@ public class LivroService {
 	}
 
 	public List<Livro> findAll(Integer id_cat) {
-		cr.findById(id_cat); // verifica se nao ide nao exite retorna o erroda excessão tratado no metodo
+		cs.findById(id_cat); // verifica se nao ide nao exite retorna o erroda excessão tratado no metodo
 								// findById()..
 		return lr.findAllByCategoria(id_cat);
 	}
@@ -50,5 +51,12 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setTesto(obj.getTesto());
 		
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null); // evita criar o id repitido
+		Categoria cat = cs.findById(id_cat); // cerifica se a categoria existe na base de dados, o metodo findById() trata disso.
+		obj.setCategoria(cat);
+		return lr.save(obj);
 	}
 }
